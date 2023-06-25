d03906eedfa3b990bb6eeb3fe4ee420ad09c225b
ugchongg <121675791+ugchongg@users.noreply.github.com>
2023-06-26 04:12:22 +0800

    updated map

--- app/(home)/map.jsx
+++ app/(home)/map.jsx
@@ -7,17 +7,25 @@ import color from '../../config/colors';
 
 import { supabase } from '../../lib/supabase';
 import { useAuth } from '../../contexts/auth';
+import CustomButton from '../../components/customButton';
+
+
+//import map components 
+import React, {useEffect} from 'react';
+import MapView, { Callout, Marker } from 'react-native-maps';
+import * as location from 'expo-location';
+
 
 function LogoTitle() {
     return (
       <Image
-        style={{ width: 30, height: 30, top: 2}}
+        style={{ width: 20, height: 20, top: 3}}
         source={require("../../assets/map.png")}
       />
     );
   }
 
-function MapPage() {
+/*function MapPage() {
     const navigation = useRouter();
     const { user } = useAuth();
     const [visible, setVisible] = useState(false);
@@ -74,6 +82,8 @@ function MapPage() {
             />
             <SafeAreaView style={styles.container}>
                 <View style = {styles.top}>
+                <CustomButton type="back" style={styles.back} onPress={() => navigation.back()}/>
+
                     <Text>map</Text>
                     <Button title='temp button for mission complete' onPress={() => setVisible(true)}/>
                     <Modal
@@ -135,5 +145,76 @@ const styles = StyleSheet.create({
         fontSize: 30,
     },
 })
+*/
+function MapPage() {
+
+    const [pin, setPin] = React.useState({
+        latitude: 1.2966, 
+        longitude: 103.7764
+    });
+/*
+    React.useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setPin(location);
+
+        })()
+    }, []);*/
+
+    return (
+        <View style={styles.container}>
+            <MapView 
+            style={styles.map} 
+                initialRegion={{
+                    latitude: 1.2966,
+                    longitude: 103.7764,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                showsUserLocation = {true} 
+                //onUserLocationChange={(e) => {
+                //    console.log("onUserLocationChange", e.nativeEvent.coordinate)
+                //    ;
+                //}}
+            >
+            <Marker
+                coordinate={{latitude : 1.2966, longitude : 103.7764}}
+                title={"National University of Singapore"}
+                description='NUS'
+                pinColor='red'
+                draggable={true}
+                onDragStart={(e) => {
+                    console.log("Drag Start", e.nativeEvent.coordinate.longitude);
+                }}
+                onDragEnd={(e) => {
+                    console.log("Drag End", e.nativeEvent.coordinate.latitude);
+                    //SetPin({
+                    //    latitude: e.nativeEvent.coordinate.latitude,
+                    //    longitude: e.nativeEvent.coordinate.longitude,
+                    //});
+                }}   
+            >
+            </Marker>
+            </MapView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+  }
+);
 
 export default MapPage;
