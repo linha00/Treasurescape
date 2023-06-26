b4dcca209b667fd8cf40c25c93828d00f37ca3cc
hao <110731318+linha00@users.noreply.github.com>
2023-06-26 09:33:44 +0800

    Map (#3)
    * Add files via upload
    
    updated map
    
    * Add files via upload
    
    updated map
    
    * Delete map.jsx
    
    * Add files via upload
    
    updated map
    
    * updated map
    
    ---------
    
    Co-authored-by: ugchongg <121675791+ugchongg@users.noreply.github.com>

--- app/(home)/map.jsx
+++ app/(home)/map.jsx
@@ -1,86 +1,84 @@
 /* eslint-disable react/no-unescaped-entities */
-import {StyleSheet, SafeAreaView, Text, View, Image, Dimensions } from 'react-native';
+import {StyleSheet, SafeAreaView, Text, Button, View, Modal, Image, TouchableWithoutFeedback } from 'react-native';
+import { useState } from 'react';
 import { Tabs } from "expo-router"
 import { useRouter } from 'expo-router';
 import color from '../../config/colors';
 
+import { supabase } from '../../lib/supabase';
+import { useAuth } from '../../contexts/auth';
 import CustomButton from '../../components/customButton';
 
-const logo = Dimensions.get('window').width / 16;
+
+//import map components 
+import React, {useEffect} from 'react';
+import MapView, { Callout, Marker } from 'react-native-maps';
+import * as location from 'expo-location';
 
 function LogoTitle() {
     return (
       <Image
-        style={{ width: logo, height: logo}}
+        style={{ width: 20, height: 20, top: 3}}
         source={require("../../assets/map.png")}
       />
     );
   }
 
 function MapPage() {
-    const nav = useRouter();
+
+    const [pin, setPin] = React.useState({
+        latitude: 1.2966, 
+        longitude: 103.7764
+    });
 
     return (
-        <>
-            <Tabs.Screen 
-                options={{
-                    tabBarIcon: () => <LogoTitle />
+        <View style={styles.container}>
+            <MapView 
+            style={styles.map} 
+                initialRegion={{
+                    latitude: 1.2966,
+                    longitude: 103.7764,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
                 }}
-            />
-            <SafeAreaView style={styles.container}>
-                <View style = {styles.top}>
-                <CustomButton type="back" style={styles.back} onPress={() => nav.back()}/>
-
-                    <Text>map</Text>
-
-                </View>
-                <View style = {styles.tab} >
-                    <Text style = {{fontSize: 20, left: 10}}>Mission & Clues</Text>
-                </View>
-            </SafeAreaView>
-        </>
-    ); 
+                showsUserLocation = {true} 
+                //onUserLocationChange={(e) => {
+                //    console.log("onUserLocationChange", e.nativeEvent.coordinate)
+                //    ;
+                //}}
+            >
+            <Marker
+                coordinate={{latitude : 1.2966, longitude : 103.7764}}
+                title={"National University of Singapore"}
+                description='NUS'
+                pinColor='red'
+                draggable={true}
+                onDragStart={(e) => {
+                    console.log("Drag Start", e.nativeEvent.coordinate.longitude);
+                }}
+                onDragEnd={(e) => {
+                    console.log("Drag End", e.nativeEvent.coordinate.latitude);
+                    //SetPin({
+                    //    latitude: e.nativeEvent.coordinate.latitude,
+                    //    longitude: e.nativeEvent.coordinate.longitude,
+                    //});
+                }}   
+            >
+            </Marker>
+            </MapView>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+      flex: 1,
     },
-
-    top: {
-        flex: 5,
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'center',
+    map: {
+      width: '100%',
+      height: '100%',
     },
-
-    tab: {
-        flex: .3,
-        width: '100%',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-        backgroundColor: color.primary,
-        borderTopEndRadius: 10,
-        borderTopStartRadius: 10,
-    },
-
-    correct: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: color.gold,
-    },
-
-    correctImage: {
-        width: 200,
-        height: 200,
-    },
-
-    correctText: {
-        fontSize: 30,
-    },
-})
+  }
+);
 
 export default MapPage;
